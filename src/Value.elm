5907cadb8faf0ca1e8b5a36d760db4080ac1bc6d
Dillon Kearns <dillon@incrementalelm.com>
2026-03-03 21:45:41 -0800

    Intern common Values (LT/EQ/GT/Nothing) to avoid repeated allocation.

--- src/Value.elm
+++ src/Value.elm
@@ -1,4 +1,4 @@
-module Value exposing (fromOrder, nameError, toArray, toExpression, toOrder, toString, todo, typeError, unsupported)
+module Value exposing (eqValue, fromOrder, gtValue, ltValue, nameError, nothingValue, toArray, toExpression, toOrder, toString, todo, typeError, unsupported)
 
 import Array exposing (Array)
 import Elm.Syntax.Expression as Expression exposing (Expression)
@@ -297,17 +297,42 @@ escapeChar c =
             String.fromChar c
 
 
+{-| Pre-computed Order values. Avoids allocating QualifiedNameRef + Custom
+on every comparison result (millions of times in fuzz tests).
+-}
+ltValue : Value
+ltValue =
+    Custom { moduleName = [ "Basics" ], name = "LT" } []
+
+
+eqValue : Value
+eqValue =
+    Custom { moduleName = [ "Basics" ], name = "EQ" } []
+
+
+gtValue : Value
+gtValue =
+    Custom { moduleName = [ "Basics" ], name = "GT" } []
+
+
+{-| Pre-computed Nothing value.
+-}
+nothingValue : Value
+nothingValue =
+    Custom { moduleName = [ "Maybe" ], name = "Nothing" } []
+
+
 fromOrder : Order -> Value
 fromOrder order =
     case order of
         LT ->
-            Custom { moduleName = [ "Basics" ], name = "LT" } []
+            ltValue
 
         EQ ->
-            Custom { moduleName = [ "Basics" ], name = "EQ" } []
+            eqValue
 
         GT ->
-            Custom { moduleName = [ "Basics" ], name = "GT" } []
+            gtValue
 
 
 toOrder : Value -> Maybe Order
